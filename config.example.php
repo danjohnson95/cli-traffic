7e5893166a39c581d3be4ba4e692896e9c89a4a3
Dan <dan@danj.eu>
2017-05-08 20:24:00 +0100

    Added example config file

---
+++ config.example.php
@@ -0,0 +1,41 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Data Source
+    |--------------------------------------------------------------------------
+    | Defines what data source should be used to collect traffic information.
+    | Available Settings: "google"
+    */
+    'data' => 'google',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Origin Postcode
+    |--------------------------------------------------------------------------
+    | The postcode from where the commute will start,
+    | Example: "E1 1AA"
+    */
+    'origin' => 'E1 1AA',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Destination Postcode
+    |--------------------------------------------------------------------------
+    | The postcode from where the commute will end,
+    | Example: "SW1 1AA"
+    */
+    'destination' => 'SW1 1AA',
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Key
+    |--------------------------------------------------------------------------
+    | An API key to access the desired API. Please see the README.md for
+    | further instructions.
+    */
+    'key' => 'put-key-here'
+
+];
+
